b40f936258c722f086e77f97c14757a1e1bae803
VolcanicArts <VolcanicArtsBP@Gmail.com>
2021-07-25 18:09:51 +0100

    Remove unneeded variable

--- Assets/VRCToolkit/VRCPackageManager/Editor/VRCPackage/VRCPackageSection.cs
+++ Assets/VRCToolkit/VRCPackageManager/Editor/VRCPackage/VRCPackageSection.cs
@@ -5,7 +5,6 @@ namespace VRCToolkit.VRCPackageManager.Editor.VRCPackage
     [Serializable]
     public class VRCPackageSection
     {
-        public string title;
         public VRCPackage[] packages;
     }
 }
