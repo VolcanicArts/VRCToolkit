dfdf9d97259f9a63048109f71625a11c2557ac3a
VolcanicArts <VolcanicArtsBP@Gmail.com>
2021-07-25 03:21:19 +0100

    Add in import logging

--- Assets/VRCToolkit/VRCPackageManager/Editor/PackageDownloader.cs
+++ Assets/VRCToolkit/VRCPackageManager/Editor/PackageDownloader.cs
@@ -18,6 +18,21 @@ namespace VRCToolkit.VRCPackageManager.Editor
             uwr = new UnityWebRequest(url);
             path = $"{Application.dataPath}/VRCToolkit/VRCPackageManager/Downloads/{fileName}";
             uwr.downloadHandler = new DownloadHandlerFile(path);
+            AssignEvents();
+        }
+
+        private void AssignEvents()
+        {
+            AssetDatabase.importPackageStarted += OnImportPackageStarted;
+            AssetDatabase.importPackageCompleted += OnImportPackageCompleted;
+            AssetDatabase.importPackageFailed += OnImportPackageFailed;
+        }
+
+        private void UnAssignEvents()
+        {
+            AssetDatabase.importPackageStarted -= OnImportPackageStarted;
+            AssetDatabase.importPackageCompleted -= OnImportPackageCompleted;
+            AssetDatabase.importPackageFailed -= OnImportPackageFailed;
         }
             
         public void ExecuteDownload()
@@ -33,7 +48,7 @@ namespace VRCToolkit.VRCPackageManager.Editor
             
             if (uwr.error == null)
             {
-                Logger.Log($"{packageName} successfully downloaded. Importing unitypackage!");
+                Logger.Log($"{packageName} has been successfully downloaded!");
                 AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
                 AssetDatabase.ImportPackage(path, false);
             }
@@ -41,6 +56,24 @@ namespace VRCToolkit.VRCPackageManager.Editor
             {
                 Logger.LogError(uwr.error);;
             }
+        }
+
+        private void OnImportPackageStarted(string ignored)
+        {
+            Logger.Log($"Attempting to import {packageName}");
+        }
+        
+        private void OnImportPackageCompleted(string ignored)
+        {
+            Logger.Log($"{packageName} has been successfully imported!");
+            UnAssignEvents();
+        }
+        
+        private void OnImportPackageFailed(string ignored, string errorMessage)
+        {
+            Logger.LogError($"Failed to import {packageName}");
+            Logger.LogError(errorMessage);
+            UnAssignEvents();
         }
     }
 }
