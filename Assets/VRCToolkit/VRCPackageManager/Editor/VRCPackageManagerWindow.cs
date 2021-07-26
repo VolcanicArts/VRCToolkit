5a0a52ccf316315980787ebbd9611052a0ca6bbc
VolcanicArts <VolcanicArtsBP@Gmail.com>
2021-07-26 02:03:31 +0100

    Clean up variable naming

--- Assets/VRCToolkit/VRCPackageManager/Editor/VRCPackageManagerWindow.cs
+++ Assets/VRCToolkit/VRCPackageManager/Editor/VRCPackageManagerWindow.cs
@@ -133,28 +133,28 @@ namespace VRCToolkit.VRCPackageManager.Editor
             }
         }
 
-        private static string GetLatestReleaseFileName(string repo, string repoName, string nameFormat)
+        private static string GetLatestReleaseFileName(string repoName, string formattedName, string nameFormat)
         {
-            var url = GitHubAPIBase + repo + GitHubAPILatestRelease;
-            Logger.Log($"Requesting for latest version of {repoName} using URL: {url}");
+            var url = GitHubAPIBase + repoName + GitHubAPILatestRelease;
+            Logger.Log($"Requesting for latest version of {formattedName} using URL: {url}");
             var uwr = new UnityWebRequest(url) {downloadHandler = new DownloadHandlerBuffer()};
             uwr.SendWebRequest();
 
             while (!uwr.isDone)
             {
-                EditorUtility.DisplayProgressBar($"[VRCPackageManager] Getting latest version of {repoName}", "", uwr.downloadProgress);
+                EditorUtility.DisplayProgressBar($"[VRCPackageManager] Getting latest version of {formattedName}", "", uwr.downloadProgress);
             }
             EditorUtility.ClearProgressBar();
 
             if (uwr.error != null)
             {
-                Logger.LogError($"Could not get latest version of {repoName}. Aborting download");
+                Logger.LogError($"Could not get latest version of {formattedName}. Aborting download");
                 return null;
             }
 
             var responseData = uwr.downloadHandler.text;
             var gitHubData = JsonUtility.FromJson<GitHubAPIResponse>(responseData);
-            var fileName = string.Format(nameFormat, repoName, gitHubData.tag_name);
+            var fileName = string.Format(nameFormat, formattedName, gitHubData.tag_name);
             Logger.Log($"Found latest version of {gitHubData.tag_name}");
             return fileName;
         }
