905193c44d93772116f4f534df41775a98e616a5
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-05-17 03:24:58 +0100

    Improve SDK screen

--- Assets/VRCToolkit/VRCPackageManager/Editor/Screen/SDKScreen.cs
+++ Assets/VRCToolkit/VRCPackageManager/Editor/Screen/SDKScreen.cs
@@ -12,16 +12,16 @@ namespace VRCToolkit.VRCPackageManager
             DrawCenteredTitle("Choose an SDK");
             DrawCenteredText("This will install the latest version of the SDK, and store the SDK type");
             DrawCenteredText("If you have an SDK installed, please choose the SDK you're using");
+            
             GUILayout.Space(10);
             DrawSDK(nameof(SDKURLs.SDK3World), SDKURLs.SDK3World);
             DrawSDK(nameof(SDKURLs.SDK3Avatar), SDKURLs.SDK3Avatar);
+            GUILayout.Space(10);
         }
 
         private static void DrawSDK(string name, string url)
         {
-            DrawCenteredTitle(name);
-            var install = DrawCenteredButton("Choose SDK");
-            GUILayout.Space(20);
+            var install = DrawCenteredButton(name);
 
             if (!install) return;
             SDKInstallHandler.InstallSDK(name, url);
